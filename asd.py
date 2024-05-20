1fdae5ac68b919769d531ec7ea2063c53d420375
Emiliano Giupponi <30704058+emigiupponi@users.noreply.github.com>
2024-05-20 20:27:46 +0000

    17:27

--- asd.py
+++
@@ -1,8 +0,0 @@
-from nbconvert.execute import execute_notebook
-
-# Ruta al archivo .ipynb que quieres ejecutar
-notebook_path = "code.ipynb"
-
-
-# Ejecuta el notebook
-execute_notebook(notebook_path)
